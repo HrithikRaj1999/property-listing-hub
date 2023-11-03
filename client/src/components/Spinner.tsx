ecb0ed0160e29f933ea24dd52d53d40f575d2b36
Hrithik Raj <write2hrtihik@gmail.com>
2023-11-04 01:18:24 +0530

    spinner adjusted

--- client/src/components/Spinner.tsx
+++ client/src/components/Spinner.tsx
@@ -2,12 +2,11 @@ const Spinner = ({ height = 6, width = 6 }) => {
   return (
     <div>
       <div
-        className={`inline-block h-${height} w-${width} animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]`}
+        className={`inline-block animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-none`}
+        style={{ height: `${height}px`, width: `${width}px` }}
         role="status"
       >
-        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-          Loading...
-        </span>
+        <span className="sr-only">Loading...</span>
       </div>
     </div>
   );
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,19 +2,32 @@ import { useEffect, useState } from "react";
 import api from "../config/customApi";
 import { itemType } from "../hooks/useShowListing";
 import AllListing from "../components/Listing/AllListing";
+import { toast } from "react-toastify";
+import Spinner from "../components/Spinner";
 
 const Home = () => {
   const [listings, setListings] = useState<itemType[] | null>([]);
+  const [loading, setLoading] = useState(false);
   const fetchData = async () => {
     try {
+      setLoading(true);
       const { data } = await api.get(`/listing/get-all-listings`);
       setListings([...data?.listings]);
-    } catch (error) {}
+      setLoading(false);
+    } catch (error: any) {
+      setLoading(true);
+      toast.error(error?.response.data.message);
+    }
   };
   useEffect(() => {
     fetchData();
   }, []);
-
+  if (loading)
+    return (
+      <div className="flex m-3 p-5 flex-wrap justify-center items-center h-screen sm:justify-center gap-6 sm:w-full ">
+        <Spinner height={60} width={60} />
+      </div>
+    );
   return (
     <div className="flex m-3 p-5 flex-wrap justify-center sm:justify-center gap-6 sm:w-full ">
       {listings && listings.length > 0 ? (
