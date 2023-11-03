cfa848797426c7731a81d9ed4608badfb228038f
Hrithik Raj <write2hrtihik@gmail.com>
2023-11-04 04:51:03 +0530

    UI

--- client/src/components/Search/FilteredListings.tsx
+++ client/src/components/Search/FilteredListings.tsx
@@ -6,67 +6,60 @@ import { Eye } from "react-feather";
 const FilteredListings = () => {
   const { values } = useFormikContext<SearchValuesType>();
   return values?.filteredListings.length > 0 ? (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-      {/* Map over your items here */}
-      {values?.filteredListings?.map((item, index) => (
-        <div
-          key={item._id}
-          className="flex flex-col bg-white rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out"
-        >
-          {/* Image and overlay */}
-          <Link to={`/show-listing/${item._id}`}>
-            <div className="relative">
-              <img
-                className="w-full h-48 object-cover"
-                src={typeof item.imageUrls[0] === "string" ? item.imageUrls[0] : ""}
-                alt={item.name}
-              />
-              <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-0 opacity-0 transition-opacity duration-300 hover:bg-opacity-50 hover:opacity-100">
-                <Eye color={"white"} size={40} />
+    <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-6">
+      {values?.filteredListings?.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {/* Map over your items here */}
+          {values.filteredListings.map((item, index) => (
+            <div
+              key={item._id}
+              className="flex flex-col bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out overflow-hidden"
+            >
+              {/* Image and overlay */}
+              <Link to={`/show-listing/${item._id}`} className="group">
+                <div className="relative overflow-hidden">
+                  <img
+                    className="w-full h-48 object-cover transform group-hover:scale-105 transition duration-300 ease-in-out"
+                    src={typeof item.imageUrls[0] === "string" ? item.imageUrls[0] : ""}
+                    alt={item.name}
+                  />
+                  <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-0 group-hover:bg-opacity-50 group-hover:opacity-100 transition-opacity duration-300">
+                    <Eye className="text-white" size={40} />
+                  </div>
+                </div>
+              </Link>
+              {/* Content */}
+              <div className="flex-grow p-4">
+                <h4 className="text-lg font-semibold text-gray-800 truncate">{item.name}</h4>
+                <p className="text-gray-600 mt-2 line-clamp-3">{item.description}</p>
+                <div className="mt-4">
+                  <p className="text-sm text-gray-500 truncate">
+                    <FaRoute className="inline mr-1" />
+                    {item.address}
+                  </p>
+                  <p className="text-sm text-gray-500 truncate">
+                    <FaHome className="inline mr-1" />
+                    {item.roomType.toUpperCase()}
+                  </p>
+                </div>
+              </div>
+              {/* Price section */}
+              <div className="px-4 py-3 bg-gray-100 flex justify-between items-center">
+                <span className="text-sm font-medium text-gray-500 line-through">
+                  ₹{item.specifications.regularPrice.toFixed(2)}
+                </span>
+                <span className="text-lg font-semibold text-green-600">
+                  ₹{item.specifications.discountedPrice.toFixed(2)}
+                </span>
               </div>
             </div>
-          </Link>
-          {/* Content */}
-          <div className="flex flex-col flex-grow p-4">
-            <h4 className="text-xl font-semibold text-gray-800 truncate">{item.name}</h4>
-            <p
-              className="flex-1 text-sm text-gray-600 mt-2 overflow-hidden"
-              style={{
-                maxHeight: "4.5rem",
-                WebkitLineClamp: 3,
-                WebkitBoxOrient: "vertical",
-                display: "-webkit-box",
-              }}
-            >
-              <FaInfoCircle className="inline mr-2" size={16} />
-              {item.description}
-            </p>
-            <div className="mt-auto">
-              <p className="text-sm text-gray-600 truncate">
-                <FaRoute className="inline mr-2" />
-                {item.address}
-              </p>
-              <p className="text-sm text-gray-600 truncate">
-                <FaHome className="inline mr-2" />
-                {item.roomType.toUpperCase()}
-              </p>
-            </div>
-          </div>
-          {/* Price section */}
-          <div className="px-4 py-3 bg-gray-100">
-            <span className="text-sm font-medium text-gray-600 line-through">
-              ₹{item.specifications.regularPrice.toFixed(2)}
-            </span>
-            <span className="text-lg font-semibold text-green-600">
-              ₹{item.specifications.discountedPrice.toFixed(2)}
-            </span>
-          </div>
+          ))}
         </div>
-      ))}
+      ) : (
+        <h1 className="text-2xl text-center text-gray-800">No Listing Found</h1>
+      )}
     </div>
-  ) : (
-    <h1 className="text-2xl text-center">No Listing Found</h1>
-  );
+  ) : null;
 };
 
 export default FilteredListings;
